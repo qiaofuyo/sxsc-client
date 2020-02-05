ff6242dff2f36b5d6b381d7cff740b6e7fb03ebd
qiaofuyo <1103351140@qq.com>
2020-02-05 16:30:48 +0800

    '蓦然回首，那人却在灯火阑珊处'

--- src/vues/pages/Home/Home.vue
+++ src/vues/pages/Home/Home.vue
@@ -4,7 +4,7 @@
 		<div class="head">
 			<!-- 选择位置，搜索框 -->
 			<!-- 轮播图 -->
-			<home-swiper-container />
+			<home-swiper-container :homeslide_data='homeslide_data' />
 		</div>
 		<!-- 中间导航 -->
 		<!-- <home-nav /> -->
@@ -18,6 +18,13 @@
 	import HomeSwiperContainer from './HomeSwiperContainer.vue'
 	// import HomeNav from './HomeNav.vue';
 	// import HomeShopList from './HomeShopList.vue';
+	
+	// 导入请求数据的api
+	import {
+		getHomeSlide,
+		// getHomeNav,
+		// getHomeShopList
+	} from '../../../api/index.js'
 
 	export default {
 		name: 'Home',
@@ -26,13 +33,33 @@
 			// HomeNav,
 			// HomeShopList
 		},
-		mounted() {
-		// 	// 分发action——请求首页轮播图
-			this.$store.dispatch('reqHomeSlide')
+		data() {
+			return {
+				homeslide_data: [],  // 轮播图组件 数据
+				// homenav_data: [],  // 中间导航组件 数据
+				// homeshoplist_data: [],  // 商品列表组件 数据
+			}
+		},
+		created() {
+			// 数据初始化
+			this._initData();
+		},
+		methods: {
+		// b站拼多多走vuex, 页面首次加载因异步原因组件获取不到数据
+		// 分发action——请求首页轮播图
+		// this.$store.dispatch('reqHomeSlide')
 		// 	// 分发action——请求首页导航
 		// 	this.$store.dispatch('reqHomeNav'),
 		// 	// 分发action——请求首页商品列表
 		// 	this.$store.dispatch('reqHomeShopList')
+			// 不走vuex
+			async  _initData () {
+				const data = await getHomeSlide()
+				if (data.message) {
+					// 保存 轮播图组件 数据
+					this.homeslide_data = data.message
+				}
+			}
 		}
 	}
 </script>
--- src/vues/pages/Home/HomeSwiperContainer.vue
+++ src/vues/pages/Home/HomeSwiperContainer.vue
@@ -3,7 +3,7 @@
 		<!-- 图片， 通过服务器请求得到的图片 -->
 		<div class="swiper-wrapper">
 			<!-- key如果修改则重新渲染img，否则复用img -->
-			<div class="swiper-slide" v-for="(slide,index) in homeSlide" :key="index">
+			<div class="swiper-slide" v-for="(slide,index) in homeslide_data" :key="index">
 				<img :src="slide.imgurl" />
 			</div>
 		</div>
@@ -17,36 +17,59 @@
 	import Swiper from 'swiper'
 	import 'swiper/dist/css/swiper.css'
 	// 导入vuex拿到 state.js 中的状态
-	import {
-		mapState
-	} from 'vuex'
+	// import {
+	// 	mapState
+	// } from 'vuex'
 
 	export default {
 		name: 'HomeSwiperContainer',
-		computed: {
-			// ...对象展开运算符——拿到状态中的轮播图图片数据，以供轮播图组件模版使用（数据服务器请求回来的）
-			// 辅助函数 mapState 把一个对象或数组里的值转化成计算属性的写法
-			...mapState(['homeSlide'])
-			// homeSlide() {
-			// 	return this.$store.state.homeSlide
-			// }
+		props:{
+			// 接受父组件传过来的数据
+			homeslide_data: Array
 		},
+		// computed: {
+		// 	// ...对象展开运算符——拿到状态中的轮播图图片数据，以供轮播图组件模版使用（数据服务器请求回来的）
+		// 	// 辅助函数 mapState 把一个对象或数组里的值转化成计算属性的写法
+		// 	...mapState(['homeSlide'])
+		// 	// homeSlide() {
+		// 	// 	return this.$store.state.homeSlide
+		// 	// }
+		// },
 		mounted() {
 			// 初始化轮播图Swiper
-			new Swiper('.swiper-container', {
-				loop: true, // 循环模式选项
-				// 切换设置
-				autoplay: {
-					delay: 3000, // 切换间隔
-					stopOnLastSlide: false, // 切换到最后一个后不停止自动切换
-					disableOnInteraction: false, // 触碰后不停止切换
-				},
-				// 如果需要分页器
-				pagination: {
-					el: '.swiper-pagination'
-				}
-			});
+			// new Swiper('.swiper-container', {
+			// 	loop: true, // 循环模式选项
+			// 	// 切换设置
+			// 	autoplay: {
+			// 		delay: 3000, // 切换间隔
+			// 		stopOnLastSlide: false, // 切换到最后一个后不停止自动切换
+			// 		disableOnInteraction: false, // 触碰后不停止切换
+			// 	},
+			// 	// 如果需要分页器
+			// 	pagination: {
+			// 		el: '.swiper-pagination'
+			// 	}
+			// });
 		},
+		watch:{
+			homeslide_data(){
+				this.$nextTick(()=>{
+					new Swiper('.swiper-container', {
+						loop: true, // 循环模式选项
+						// 切换设置
+						autoplay: {
+							delay: 3000, // 切换间隔
+							stopOnLastSlide: false, // 切换到最后一个后不停止自动切换
+							disableOnInteraction: false, // 触碰后不停止切换
+						},
+						// 如果需要分页器
+						pagination: {
+							el: '.swiper-pagination'
+						}
+					});
+				})
+			}
+		}
 	}
 </script>
 
