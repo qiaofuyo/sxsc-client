f027eab6f179382a811ca687677cfbecefbbef4b
liyong <1103351140@qq.com>
2021-06-23 22:51:28 +0800

    重推

--- src/common/OTO.js
+++ src/common/OTO.js
@@ -4,8 +4,8 @@
 // export const PATH = 'http://localhost:3000'
 
 // 本地测试nginx => 映射虚拟主机 192.168.1.3 test.com
-// export const PATH = 'http://test.com:3000'
+export const PATH = 'http://test.com'
 
 // 使用云端服务器
 // export const PATH = 'http://xinggui.top'
-export const PATH = '39.107.221.146'
+// export const PATH = 'http://39.107.221.146'
